803def2bd187c7ef34afc8b5d2dd22b9e6c4c5ba
somit <somitgond2@gmail.com>
2025-01-30 09:44:14 +0530

    optimizations

--- sqrt.py
+++ sqrt.py
@@ -1,9 +1,15 @@
 
+tol = 1e-14
+
 def my_sqrt(x, kmax):
     s = 1.0
     for k in range(kmax):
+        print(f"At each iteration {k} the value of s = {s}")
+        s0 = s
         s = 0.5 * (s + (x/s))
+        if(abs(s0-s) < tol):
+            break
     return s
-
-print(my_sqrt(2.0, 100))
+x = 2.0
+print(f"square root of {x} is {my_sqrt(x, 100)}")
 
